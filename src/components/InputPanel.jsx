9fcf8ce138b1419dcb98c2a08731fab12522537c
kellyyi2 <kellyyi2@illinois.edu>
2025-09-04 22:58:48 -0500

    Added original serving input

--- src/components/InputPanel.jsx
+++ src/components/InputPanel.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react"
 import IngredientDisplay from "./IngredientDisplay";
 import AddIngredientButton from "./AddIngredientButton";
-import NumPeopleInput from "./NumPeopleInput";
+import NewServings from "./NewServings";
 import ScaleButton from "./Scalebutton";
+import OriginalServings from "./OriginalServings.jsx";
 
   export default function InputPanel() {
     const [showForm, setShowForm] = useState(false);
             return (
             <section className="flex flex-col justify-start items-center flex-1 border-box max-w-1/2 p-[3%] justify-center gap-[3%] rounded-2xl font-['Orbit'] bg-[rgb(118,65,65)]">
                 <IngredientDisplay showForm={showForm} setShowForm={setShowForm} />
-                <div id="inputs" className="flex flex-col justify-evenly w-full h-full">
-                    <div id="recipeInputs" className="flex justify-between w-full">
+                <div id="inputs" className="flex flex-col items-center justify-evenly w-full h-full">
                         <AddIngredientButton onClick={() => setShowForm(true)} />
-                        <div id="peopleInput" className="flex items-center gap-[2%]">
-                            <span style={{color: "white", fontFamily:"'Orbit', sans-serif", fontWeight:400, fontSize:"0.75rem"}}>This feeds:</span>
-                            <NumPeopleInput />
+                        <div id="servingInputs" className="flex justify-between w-full">
+                            <div id="originalServings" className="flex items-center gap[2%]">
+                                <span style={{color: "white", fontFamily:"'Orbit', sans-serif", fontWeight:400, fontSize:"0.8rem"}}>Original Servings: </span>
+                                <OriginalServings />
+                            </div>
+                            <div id="peopleInput" className="flex items-center gap-[2%]">
+                                <span style={{color: "white", fontFamily:"'Orbit', sans-serif", fontWeight:400, fontSize:"0.8rem"}}>I want to feed: </span>
+                                <NewServings />
+                            </div>
                         </div>
-                    </div>
                     <ScaleButton />
                 </div>
             </section>
---
+++ src/components/NewServings.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function NewServings() {
+    return (
+        <input 
+        className="w-9 h-6 p-[0.25rem] rounded-md text-sm bg-[rgb(229,104,116)]"
+        type="text"
+        placeholder="1"/>
+    );
+}
+
